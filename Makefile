41055a3397c95e33307422813c1124dc16ee05f3
alexander <github@systemstart.net>
2026-03-19 14:09:10 +0100

    feat: release stuff

--- Makefile
+++ Makefile
@@ -4,10 +4,15 @@ export NIXPKGS_ALLOW_UNFREE := 1
 
 WORK_DIR ?= $(shell pwd)
 
-.PHONY: vm vm.run
+.PHONY: vm vm.run tag
 
 vm:
 	nix build $(NIX_FLAGS) .#vm
 
 vm.run: vm
 	WORK_DIR=$(WORK_DIR) ./result/bin/microvm-run
+
+tag:
+	$(eval VERSION ?= $(shell gsemver bump))
+	git tag -a "v$(VERSION)" -m "Release v$(VERSION)"
+	git push origin "v$(VERSION)"
--- flake.nix
+++ flake.nix
@@ -145,6 +145,43 @@
         ];
       };
 
+      devShells.${system}.default = let
+        gsemver = let
+          version = "0.10.0";
+          sources = {
+            "x86_64-linux" = {
+              url = "https://github.com/arnaud-deprez/gsemver/releases/download/v${version}/gsemver_${version}_linux_amd64.tar.gz";
+              hash = "sha256-F1oyytHMSEBZTNWVyxKM6Zua2sJeQjQ3pyyPDxYDk78=";
+            };
+            "aarch64-linux" = {
+              url = "https://github.com/arnaud-deprez/gsemver/releases/download/v${version}/gsemver_${version}_linux_arm64.tar.gz";
+              hash = "sha256-PRIp6ti87aoLoKdLWnDSJLUw+uM95olpUB2ILSmtMII=";
+            };
+            "x86_64-darwin" = {
+              url = "https://github.com/arnaud-deprez/gsemver/releases/download/v${version}/gsemver_${version}_darwin_amd64.tar.gz";
+              hash = "sha256-BBKey/Gk1gDQ3uKWuBLuPqEYdjBxxVYsBytBFOOygz4=";
+            };
+            "aarch64-darwin" = {
+              url = "https://github.com/arnaud-deprez/gsemver/releases/download/v${version}/gsemver_${version}_darwin_arm64.tar.gz";
+              hash = "sha256-kH11CbkodKKWu9Nh3piGrdTAzSOV/o4Q24uzhasQUQU=";
+            };
+          };
+          src = sources.${pkgs.stdenv.hostPlatform.system};
+        in pkgs.stdenv.mkDerivation {
+          pname = "gsemver";
+          inherit version;
+          src = pkgs.fetchurl { inherit (src) url hash; };
+          sourceRoot = ".";
+          dontConfigure = true;
+          dontBuild = true;
+          installPhase = ''
+            install -Dm755 gsemver $out/bin/gsemver
+          '';
+        };
+      in pkgs.mkShell {
+        buildInputs = [ gsemver ];
+      };
+
       packages.${system} = rec {
         default = vm;
 
